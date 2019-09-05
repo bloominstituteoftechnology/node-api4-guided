84316cf52b6533d33c87049ef2888eb63a3882e4
Avni Patel Thompson <avnipatelthompson@Avnis-iMac.local>
2019-09-05 09:44:10 -0700

    add dotenv

--- api/server.js
+++ api/server.js
@@ -1,12 +1,14 @@
-const express = require('express');
-const helmet = require('helmet');
+const express = require("express");
+const helmet = require("helmet");
 
-const apiRouter = require('./api-router.js');
+console.log("environment", process.env.NODE_ENV);
+
+const apiRouter = require("./api-router.js");
 
 const server = express();
 
 server.use(helmet());
 
-server.use('/api', apiRouter);
+server.use("/api", apiRouter);
 
 module.exports = server;
