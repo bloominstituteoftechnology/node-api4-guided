f29bc2f794b60b0eae30dd90a4ecddeec9bc382f
Adam Hoffman <wintros@yahoo.com>
2019-09-05 10:43:48 -0600

    fail

--- api/server.js
+++ api/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const helmet = require('helmet');
 
+console.log('environemt', process.env.NODE_ENV)
+
 const apiRouter = require('./api-router.js');
 
 const server = express();
