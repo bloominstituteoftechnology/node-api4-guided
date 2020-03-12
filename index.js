ad9bc3c2b2c507866f79ee7f4492a97cd8526173
Luis Hernandez <luishrd@gmail.com>
2020-03-12 11:48:05 -0400

    updates dependencies and port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const server = require('./api/server.js');
+const server = require("./api/server.js");
 
-const port = 4000;
+const port = 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
