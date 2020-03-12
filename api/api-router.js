f7a34d85b14840cab6121847dc7b71b6b26cef87
alexis villaraza <alexis.borja@gmail.com>
2020-03-12 09:42:59 -0700

    coded for heroku deployment

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,9 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', (req, res) => {
-  res.status(200).json({ api: 'up' });
+  const environment = process.env;
+  const port = process.env.PORT || 5000;
+  res.status(200).json({ api: 'up', port, environment });
 });
 
 router.get('/shouts', (req, res, next) => {
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+//have the server assign the port
+//heroku will place the .PORT environment value on their server
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+//add start script to package.json that uses node to run the API
