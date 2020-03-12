b2ab4a66f3fd0dad89367d7cef584fd8a2280a89
Seth Cox <Sethcox16@yahoo.com>
2020-03-12 12:35:36 -0400

    adds server functionality

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
