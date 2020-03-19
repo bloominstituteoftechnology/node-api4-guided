4d3217cdbee1d358a4e4cfdd60fd1f12bb068b7f
Sean Birmingham <seannbirmingham98@gmail.com>
2020-03-18 23:06:53 -0400

    added shouts

--- api/api-router.js
+++ api/api-router.js
@@ -7,8 +7,14 @@ const router = express.Router();
 router.use(express.json());
 
 router.get('/', (req, res) => {
-  const messageOfTheDay = process.env.MOTD || 'Hello World!';
-  res.status(200).json({ message: messageOfTheDay });
+  Shouts.find()
+    .then(shouts => {
+      const messageOfTheDay = process.env.MOTD || 'Hello World!';
+      res.status(200).json({ message: messageOfTheDay, shouts });
+    })
+    .catch(err => {
+      res.status(500).json((message: err));
+    });
 });
 
 router.get('/shouts', (req, res, next) => {
