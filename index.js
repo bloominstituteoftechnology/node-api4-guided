76fe84ce8e9429386a13412c50ed00305445370e
tauan-longaretti <tauanlongaretti@gmail.com>
2020-03-19 22:12:05 -0400

    notes

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+require('dotenv').config();
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
+// export PORT=6000
+// unset PORT
+// const port = 6000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// CI/CD pipeline stages:
+// * development (dev)
+// * test
+// * staging
+// * --- manual gate
+// * production
