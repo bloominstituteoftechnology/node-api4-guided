48871bee9051ce2f67643949ab511e4f4cd6e28c
M Groesbeck <m.groesbeck@gmail.com>
2020-04-09 09:38:57 -0700

    Dynamic port, node start script.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+// make port dynamic (so heroku can assign)
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
