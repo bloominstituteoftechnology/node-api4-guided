0dbf0a4a123ba6065989dd9e59fee1b864987acb
robertelias <robert.elias@live.com>
2020-05-07 09:25:51 -0700

    updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
 
-const port = 4000;
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
