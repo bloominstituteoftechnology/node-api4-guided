b04459e837b92f7a529fe959f5e4163a005691b7
Victoria  Topham <topham.victoria@gmail.com>
2020-05-07 09:21:28 -0700

    updated port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config()
 const server = require("./api/server.js");
 
 
-const port = process.env.PORT;
+const port = process.env.PORT || 4000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
