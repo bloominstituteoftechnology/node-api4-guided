7a155a67fdeeb7c394ca08360338a356f1f5ab4c
robertelias <robert.elias@live.com>
2020-05-07 10:19:07 -0700

    UPDATES AGAIN

--- knexfile.js
+++ knexfile.js
@@ -17,14 +17,14 @@ module.exports = {
       directory: './data/seeds',
     },
   },
-  production: {
-    client: "pg", // npm i pg
-    connection: process.env.DATABASE_URL, // provided by Heroku
-    migrations: {
-        directory: "./data/migrations",
-    },
-    seeds: {
-        directory: "./data/seeds",
-    },
-},
+//   production: {
+//     client: "pg", // npm i pg
+//     connection: process.env.DATABASE_URL, // provided by Heroku
+//     migrations: {
+//         directory: "./data/migrations",
+//     },
+//     seeds: {
+//         directory: "./data/seeds",
+//     },
+// },
 };
