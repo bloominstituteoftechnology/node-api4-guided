8cac9df3634138661925dd7165fa9e200fa1a645
robertelias <robert.elias@live.com>
2020-05-07 09:57:04 -0700

    updates to updates

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./api/server.js');
 
-const PORT = process.env.PORT || 4000;
-server.listen(PORT, () => {
-  console.log(`\n*** Server Running on http://localhost:${PORT} ***\n`);
+const port = process.env.PORT || 4000;
+server.listen(port, () => {
+  console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
