bbb10c9d69aeef80f0d4afe72e7a399648fd71c7
robertelias <robert.elias@live.com>
2020-05-07 10:38:28 -0700

    added restricted middleware

--- api/api-router.js
+++ api/api-router.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const Shouts = require("../shouts/shouts-model.js");
-
+const restricted = require("../auth/restricted-middleware.js")
 const router = express.Router();
 
 router.use(express.json());
@@ -18,7 +18,7 @@ router.get("/shouts", (req, res, next) => {
     .catch(error => next(error));
 });
 
-router.post("/shouts", (req, res, next) => {
+router.post("/shouts", restricted, (req, res, next) => {
   Shouts.add(req.body)
     .then(shout => {
       res.status(201).json(shout);
---
+++ auth/restricted-middleware.js
@@ -0,0 +1,11 @@
+module.exports = (req, res, next) => {
+    const {authorization} = req.headers;
+
+    const API_SECRET = process.env.API_SECRET;
+
+    if(authorization === API_SECRET ) {
+        next();
+    } else {
+        res.status(403).json({ you: "have no power in here"})
+    }
+}
