3ccb667363148ac49e625a17fec19a6157222be5
unknown <m_fattih@hotmail.com>
2020-05-14 16:13:37 -0700

    deployingHeroku

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config(); 
 const server = require("./api/server.js");
 
-const port = 5000;
+//console.log(process.env);
+
+
+
+const port = process.env.PORT ||  5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
