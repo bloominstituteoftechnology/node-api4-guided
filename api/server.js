ff38d6d838d59f0153118624f8e0b7060ec511e5
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-04 12:06:30 -0500

    testing deploy

--- api/server.js
+++ api/server.js
@@ -7,6 +7,10 @@ const server = express();
 
 server.use(helmet());
 
+server.get('/', function (req, res) {
+    res.status(200).json({ environment: process.env.NODE_ENV })
+})
+
 server.use('/api', apiRouter);
 
 module.exports = server;
