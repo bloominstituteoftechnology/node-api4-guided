af0966c37618d6c8556960b7ec66d45d7371d695
Guillermo Alfaro <galfarotolon@gmail.com>
2020-06-04 11:45:21 -0500

    testing deploy

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require("./api/server.js");
 
+//make the port dynamic
+
 const port = process.env.PORT || 8000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
