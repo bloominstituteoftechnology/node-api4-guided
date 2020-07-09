42b0433c36ed65f68a95d8c34e229e91282b1dae
jasonelliots <j.elliotschwartz@gmail.com>
2020-07-09 09:42:41 -0700

    started

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const server = require("./api/server.js");
 
-const port = 5000;
+console.log("node environment")
+
+const port = process.env.PORT || 5000;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
