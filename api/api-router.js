ef9ca5922e02b475ee35bb4fffcbe4de00460dbe
jasonelliots <j.elliotschwartz@gmail.com>
2020-07-09 10:09:36 -0700

    updated message

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const message = process.env.MESSAGE; 
+  res.status(200).json({ message });
 });
 
 router.get("/shouts", (req, res, next) => {
