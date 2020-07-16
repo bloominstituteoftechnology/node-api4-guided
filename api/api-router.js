6fc98e67edab8cd23188c92aab873b61850f052d
Nathan Brown <scottynate05@yahoo.com>
2020-07-15 20:22:53 -0700

    added motd

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const messageOfTheDay = process.env.MOTD || 'Hello World!'
+  res.status(200).json({ api: "up", motd: messageOfTheDay });
 });
 
 router.get("/shouts", (req, res, next) => {
