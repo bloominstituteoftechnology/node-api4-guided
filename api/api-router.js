4c5079a8c3a2f69cd1b44c3ff08bcd13e5b4f40e
JenVest2020 <jenvest2019@gmail.com>
2020-09-09 22:51:24 -0500

    moved dotenv to dependencies

--- api/api-router.js
+++ api/api-router.js
@@ -7,7 +7,8 @@ const router = express.Router();
 router.use(express.json());
 
 router.get("/", (req, res) => {
-  res.status(200).json({ api: "up" });
+  const motd = process.env.MOTD || 'Hello World';
+  res.status(200).json({ api: "up", motd: motd });
 });
 
 router.get("/shouts", (req, res, next) => {
