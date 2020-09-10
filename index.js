9adf08b400c92308d20e3a38306fbce211181844
JenVest2020 <jenvest2019@gmail.com>
2020-09-09 21:41:38 -0500

    created start script and dynamic port

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
