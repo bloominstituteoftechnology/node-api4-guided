e00e648429c5f1447ea14e523663650dba8fc08c
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-10-29 18:00:49 +0100

    wip

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
+require('dotenv').config()
+
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT || 4000;
+const secret = process.env.SECRET_THING || 'foo';
+
+console.log(port, secret);
+
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
