46415008d84cc1fd882b856fff228abb2f377beb
Alan Mir <alanpaulphotos@gmail.com>
2021-03-17 23:01:54 -0400

    Added a message of the day

--- api/server.js
+++ api/server.js
@@ -9,4 +9,9 @@ server.use(helmet());
 
 server.use('/api', apiRouter);
 
+server.get('/motd', (req, res) => {
+    const motd = process.env.MOTD || "Hello World!";
+    res.status(200).json({ motd = motd });
+})
+
 module.exports = server;
