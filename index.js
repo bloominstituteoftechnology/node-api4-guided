9c450e5a08847475b6d5ca6bfa96d15f89442261
Alan Mir <alanpaulphotos@gmail.com>
2021-03-17 22:31:58 -0400

    made it dynamic

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
+require('dotenv').config();
+
 const server = require("./api/server.js");
 
-const port = 5000;
+const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`\n*** Server Running on http://localhost:${port} ***\n`);
 });
+
+// CI / CD
+// DEV ==> TEST ==> STAGING ==> PRODUCTION
